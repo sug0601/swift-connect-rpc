7d3c278a481cc51eb7f1c5fd75f48a43d2165d29
sug0601 <sug0601@live.jp>
2025-10-05 14:15:20 +0900

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by 三輪貴之 on 2025/10/05.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ testTests/testTests.swift
@@ -0,0 +1,16 @@
+//
+//  testTests.swift
+//  testTests
+//
+//  Created by 三輪貴之 on 2025/10/05.
+//
+
+import Testing
+
+struct testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
